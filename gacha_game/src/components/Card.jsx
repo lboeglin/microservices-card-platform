24c53d16f00ba4cfa02fc8a41f6ce33e55cdeb5b
samuel <samuelamaralantunes@gmail.com>
2025-04-02 17:27:11 +0200

    tweak: card adjustments

--- gacha_game/src/components/Card.jsx
+++ gacha_game/src/components/Card.jsx
@@ -11,7 +11,6 @@ export default ({ name = 'Unnamed', rarity = Rarity.COMMON, artwork = '', goofy 
 	const [tilt, setTilt] = useState({ x: 0, y: 0 })
 	const height = 290
 	const width = 180
-	const light = { x: -1, y: -1 }
 	const card = (
 		<div
 			style={goofy ? {
@@ -22,10 +21,10 @@ export default ({ name = 'Unnamed', rarity = Rarity.COMMON, artwork = '', goofy 
 			} : {
 				width: `${width}px`,
 				height: `${height}px`,
-				transform: `perspective(120rem) rotate3D(${-tilt.y
-					}, ${tilt.x}, 0, ${Math.sqrt(tilt.x ** 2 + tilt.y ** 2) / 7}deg)`,
+				transform: `perspective(500px) rotate3D(${-tilt.y
+					}, ${tilt.x}, 0, ${Math.sqrt(tilt.x ** 2 + tilt.y ** 2) / 10}deg)`,
 				transformStyle: 'preserve-3d',
-				boxShadow: `${(tilt.x * 10) / width / 2}px ${(tilt.y * 10) / height / 2}px 10px #000000a2`
+				boxShadow: `${(tilt.x * 20) / width / 2}px ${(tilt.y * 20) / height / 2}px 8px #00000042`
 			}}
 			onMouseMove={(e) => {
 				const rect = e.currentTarget.getBoundingClientRect()
@@ -35,22 +34,22 @@ export default ({ name = 'Unnamed', rarity = Rarity.COMMON, artwork = '', goofy 
 			}}
 			onMouseLeave={(e) => { setTilt({ x: 0, y: 0 }) }}
 			className={"relative bg-blue-400 text-white text-center rounded flex flex-col hover:z-10 ease-out duration-400 transition-all hover:scale-[1.10]"}>
-			<img className="object-cover w-full h-full rounded flex-grow" src={artwork} />
-			<div className="m-1">
+			<img className="object-cover object-top w-full h-full rounded" src={artwork} />
+			<div className="absolute left-0 bottom-0 backdrop-blur-lg rounded-b bg-slate-800/75 text-center w-full p-2">
 				<h1 className="font-bold">
 					{name}
 					<br />
 					{"⭐".repeat(rarity + 1 || 1)}
 				</h1>
-				<div
-					style={{
-						background: '#ffffffa0',
-						opacity: Math.max(light.x * tilt.x / width / 2 + light.y * tilt.y / height / 2, 0),
-						//background: `radial-gradient(circle at ${(tilt.x + width / 2) / width * 100}% ${(tilt.y + height / 2) / height * 100}%, #ffffff2a 0%, #ffffff02 100%)`,
-						mixBlendMode: 'color-dodge'
-					}}
-					className="absolute top-0 left-0 w-full h-full ease-out transition-opacity">
-				</div>
+			</div>
+			<div
+				style={{
+					//background: '#ffffffa0',
+					opacity: Math.max(Math.abs(tilt.x) / (width / 2), Math.abs(tilt.y) / (height / 2)),
+					background: `radial-gradient(circle at ${(tilt.x + width / 2) / width * 100}% ${(tilt.y + height / 2) / height * 100}%, #ffffff2a 0%, #ffffff12 100%)`,
+					mixBlendMode: 'lighten'
+				}}
+				className="absolute top-0 left-0 w-full h-full ease-out transition-opacity">
 			</div>
 		</div >
 	)
